53308d75034a544116120c655374a49285ee54ea
Benny Hung <wakenightryu@gmail.com>
2020-10-13 16:30:38 -0400

    Dryed up UserVideos Controller with current_user, find_video, find_user_video before_action

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -1,15 +1,10 @@
 class UserVideosController < ApplicationController
+    before_action :current_user, :find_video, :find_user_video
 
     def comment
-        current_user
-        find_video
-        find_user_video 
     end
 
     def update 
-        current_user
-        find_video
-        find_user_video
         @user_video.comment=params[:user_video][:comment]
         if @user_video.update(user_video_params)
             render :comment 
